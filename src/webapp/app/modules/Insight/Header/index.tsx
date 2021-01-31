db79724c0f7302b215ec3b584eec9078c4aac61d
Renan <rollorenan@gmail.com>
2021-01-31 19:27:09 -0300

    adjust header for insight page

--- src/webapp/app/modules/Insight/Header/index.tsx
+++ src/webapp/app/modules/Insight/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ThemeSwitcher from '../../../components/ThemeSwitch';
-import { Container, Content } from './styles';
+import { Container, Content, Title } from './styles';
 
 const Header: React.FC = () => {
   return (
     <Container>
       <Content>
+        <Title>INBOX</Title>
         <ThemeSwitcher />
       </Content>
     </Container>

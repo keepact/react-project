7c897a74377abab49abfb62149596edb666e686c
Renan <rollorenan@gmail.com>
2021-01-31 19:27:53 -0300

    insight page components

--- src/webapp/app/modules/Insight/InsightPage.tsx
+++ src/webapp/app/modules/Insight/InsightPage.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import RouteNames from '../../shared/enums/routeNames.enum';
+// import { Link } from "react-router-dom";
+// import RouteNames from '../../shared/enums/routeNames.enum';
 import Header from './Header';
-// import { Container } from './styles';
+import Tabs from './Tabs';
+import Card from './Card';
 
 const Insight: React.FC = () => {
   return (
-    <div>
+    <>
       <Header />
-      <h1>PageInsight</h1>
-      <Link to={RouteNames.insightDetails}>clique aqui</Link>
-    </div>
+      <Tabs />
+      <Card />
+    </>
   );
 }
 

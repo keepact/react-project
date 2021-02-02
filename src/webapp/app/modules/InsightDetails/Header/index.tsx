7498b693cfba3a721ad091141d40f81624b598bf
Renan <rollorenan@gmail.com>
2021-02-02 10:01:47 -0300

    using dynamic title on insightsDetails

--- src/webapp/app/modules/InsightDetails/Header/index.tsx
+++ src/webapp/app/modules/InsightDetails/Header/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { FiArrowLeft } from "react-icons/fi";
 import ThemeSwitcher from '../../../components/ThemeSwitch';
 import { Container, Content, Title } from './styles';
 import RouteNames from '../../../shared/enums/routeNames.enum';
+import { ApplicationState } from '../../../shared/store';
 
 const Header: React.FC = () => {
+  const { data, error, loading } = useSelector(
+    (state: ApplicationState) => state.insightDetails,
+  );
+
   const history = useHistory();
   const handleChangePage = () => {
     history.push({ pathname: RouteNames.insight });
@@ -16,7 +22,7 @@ const Header: React.FC = () => {
         <button type="button" onClick={handleChangePage}>
           <FiArrowLeft />
         </button>
-        <Title>Spending Analysis</Title>
+        <Title>{data.title}</Title>
         <ThemeSwitcher />
       </Content>
     </Container>

ee856bda1fcf7a9e6de9c81e5d28e105e4840543
Renan <rollorenan@gmail.com>
2021-02-01 21:42:13 -0300

    insightDetails page first step

--- src/webapp/app/modules/InsightDetails/InsightDetailsPage.tsx
+++ src/webapp/app/modules/InsightDetails/InsightDetailsPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import RouteNames from '../../shared/enums/routeNames.enum';
+import Header from '../InsightDetails/Header';
 // import { Container } from './styles';
 
 const InsightDetails: React.FC = () => {
   return (
-    <div>
-      <h1>PageInsightDetails</h1>
-      <Link to={RouteNames.insight}>clique aqui</Link>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 

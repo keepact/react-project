13df006fdbb92eefb8006e74e0fe14fd341a4044
Renan Rollo <renan.lima.rollo@accenture.com>
2021-02-05 13:53:57 -0300

    fix backbutton when used by uread details

--- src/webapp/app/modules/InsightDetails/Header/index.tsx
+++ src/webapp/app/modules/InsightDetails/Header/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { FiArrowLeft } from "react-icons/fi";
 import ThemeSwitcher from '../../../components/ThemeSwitch';
 import { Container, Content, Title } from './styles';
 import RouteNames from '../../../shared/enums/routeNames.enum';
+import { InsightTypes } from '../../../shared/store/ducks/insight/types';
 import { ApplicationState } from '../../../shared/store';
 
 const Header: React.FC = () => {
@@ -13,7 +14,10 @@ const Header: React.FC = () => {
   );
 
   const history = useHistory();
+  const dispatch = useDispatch();
+
   const handleChangePage = () => {
+    dispatch({ type: InsightTypes.FILTER_ALL });
     history.push({ pathname: RouteNames.insight });
   }
   return (
